1e4be6dc3063921e8dc0b0c584e4cab059c80449
Rahul Shaw <rahul.k.shaw27@gmail.com>
2025-11-27 00:27:03 +0530

    minor changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('smartstore');
-  protected readonly formValue = '';
-
-  count = 0;
-}
+export class App {}
